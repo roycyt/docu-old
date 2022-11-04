4bc47b960fab610ad68a33c62bd7028f54cc58bb
Roy Chen <roycyt@gmail.com>
2022-11-04 22:24:14 +0800

    docusaurus.config.js
    Fix url and baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://roycyt.github.io',
+  baseUrl: '/docu',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
